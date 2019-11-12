10ebfac19bfd256453cc40f67c8e090d32055814
pengwork <31396670+pengwork@users.noreply.github.com>
2019-11-12 17:17:34 +0800

    更新基于workRush和RangeRush的AI

--- NewAI.java
+++ NewAI.java
@@ -2,12 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ai;
+package ai.abstraction;
 
-import ai.abstraction.AbstractAction;
-import ai.abstraction.AbstractionLayerAI;
-import ai.abstraction.Harvest;
-import ai.abstraction.WorkerRush;
 import ai.abstraction.pathfinding.AStarPathFinding;
 import ai.core.AI;
 import ai.abstraction.pathfinding.PathFinding;
@@ -21,7 +17,6 @@ import rts.PhysicalGameState;
 import rts.Player;
 import rts.PlayerAction;
 import rts.units.*;
-import weka.core.stopwords.Null;
 
 /**
  *
@@ -32,6 +27,8 @@ public class NewAI extends AbstractionLayerAI {
     protected UnitTypeTable utt;
     UnitType workerType;
     UnitType baseType;
+    UnitType barracksType;
+    UnitType rangedType;
 
     // Strategy implemented by this class:
     // If we have more than 1 "Worker": send the extra workers to attack to the nearest enemy unit
@@ -57,40 +54,47 @@ public class NewAI extends AbstractionLayerAI {
         if (utt!=null) {
             workerType = utt.getUnitType("Worker");
             baseType = utt.getUnitType("Base");
+            barracksType = utt.getUnitType("Barracks");
+            rangedType = utt.getUnitType("Ranged");
         }
     }
 
 
     public AI clone() {
-        return new NewAI(utt, pf);
+        return new WorkerRush(utt, pf);
     }
 
     public PlayerAction getAction(int player, GameState gs) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
         Player p = gs.getPlayer(player);
         PlayerAction pa = new PlayerAction();
-//        System.out.println("LightRushAI for player " + player + " (cycle " + gs.getTime() + ")");
+//        System.out.println("NewAI for player " + player + " (cycle " + gs.getTime() + ")");
 
-        // 遍历结点类型，判断是对方的，根据对方的结点属性
-        // 为我方镜像对称的位置添加相同的类型，并且执行相同的动作。
-        for(Unit u:pgs.getUnits()){
-            if(u.getPlayer() != player && )
-        }
         // behavior of bases:
         for(Unit u:pgs.getUnits()) {
             if (u.getType()==baseType &&
                     u.getPlayer() == player &&
                     gs.getActionAssignment(u)==null) {
-                baseBehavior(u,p,pgs);//u是遍历的某个unit，p是我方的payler的id，pgs是棋盘状态
+                baseBehavior(u,p,pgs);
             }
+        }
+        // behavior of barracks:
+        for (Unit u : pgs.getUnits()) {
+            if (u.getType() == barracksType
+                    && u.getPlayer() == player
+                    && gs.getActionAssignment(u) == null) {
+                barracksBehavior(u, p, pgs);
+            }
+        }
+
+        // behavior of melee units:
+        for(Unit u:pgs.getUnits()) {
             if (u.getType().canAttack && !u.getType().canHarvest &&
                     u.getPlayer() == player &&
                     gs.getActionAssignment(u)==null) {
                 meleeUnitBehavior(u,p,gs);
             }
         }
-
-        // behavior of melee units:
 
         // behavior of workers:
         List<Unit> workers = new LinkedList<Unit>();
@@ -106,15 +110,24 @@ public class NewAI extends AbstractionLayerAI {
         return translateActions(player,gs);
     }
 
-
+    // WR 对比 RR 这里区别在于 WR一直产生worker， 而RR只产生一个worker
     public void baseBehavior(Unit u,Player p, PhysicalGameState pgs) {
-        if (p.getResources()>=workerType.cost) train(u, workerType);
+        int nworkers = 0;
+        for (Unit u2 : pgs.getUnits()) {
+            if (u2.getType() == workerType
+                    && u2.getPlayer() == p.getID()) {
+                nworkers++;
+            }
+        }
+        if (p.getResources()>=workerType.cost && nworkers < 5)
+            train(u, workerType);
     }
 
     public void meleeUnitBehavior(Unit u, Player p, GameState gs) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
         Unit closestEnemy = null;
         int closestDistance = 0;
+        // 与WD的区别是，不去攻击对方距离自己的base近的结点来实现defense
         for(Unit u2:pgs.getUnits()) {
             if (u2.getPlayer()>=0 && u2.getPlayer()!=p.getID()) {
                 int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
@@ -129,21 +142,34 @@ public class NewAI extends AbstractionLayerAI {
         }
     }
 
+    public void barracksBehavior(Unit u, Player p, PhysicalGameState pgs) {
+        if (p.getResources() >= rangedType.cost) {
+            train(u, rangedType);
+        }
+    }
     public void workersBehavior(List<Unit> workers,Player p, GameState gs) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
         int nbases = 0;
+        int nbarracks = 0;
         int resourcesUsed = 0;
-        Unit harvestWorker = null;
+        List<Unit> hh = new LinkedList<>();
+        Unit harvestWorker1 = null;
         List<Unit> freeWorkers = new LinkedList<Unit>();
         freeWorkers.addAll(workers);
 
         if (workers.isEmpty()) return;
 
+        // 计算base 数目
+        // 没有的话新建base
+        // 和其他rush的区别 是这个部分不计算barrack
         for(Unit u2:pgs.getUnits()) {
             if (u2.getType() == baseType &&
                     u2.getPlayer() == p.getID()) nbases++;
+            if(u2.getType() == barracksType &&
+                    u2.getPlayer() == p.getID()){
+                nbarracks++;
+            }
         }
-
         List<Integer> reservedPositions = new LinkedList<Integer>();
         if (nbases==0 && !freeWorkers.isEmpty()) {
             // build a base:
@@ -153,11 +179,28 @@ public class NewAI extends AbstractionLayerAI {
                 resourcesUsed+=baseType.cost;
             }
         }
+        if (nbarracks == 0 && !freeWorkers.isEmpty()){
+            //建立一个兵营
+            if (p.getResources() >= barracksType.cost + resourcesUsed){
+                Unit u = freeWorkers.remove( 0);//返回从freeworkers 中移除成为执行建立兵营任务的 worker
+                buildIfNotAlreadyBuilding(u,barracksType, u.getX(), u.getY(), reservedPositions, p, pgs);
+                resourcesUsed += barracksType.cost;
+            }
+        }
 
-        if (freeWorkers.size()>0) harvestWorker = freeWorkers.remove(0);
+        // 增加一个harvest，从而产生积累而非一直消耗
+        if (freeWorkers.size()>0)
+            harvestWorker1 = freeWorkers.remove(0);
+        hh.add(harvestWorker1);
+        if (freeWorkers.size()>0)
+            harvestWorker1 = freeWorkers.remove(0);
+        hh.add(harvestWorker1);
+
 
         // harvest with the harvest worker:
-        if (harvestWorker!=null) {
+
+        for(Unit harvestWorker: hh){
+            if (harvestWorker!=null) {
             Unit closestBase = null;
             Unit closestResource = null;
             int closestDistance = 0;
@@ -189,7 +232,7 @@ public class NewAI extends AbstractionLayerAI {
                     harvest(harvestWorker, closestResource, closestBase);
                 }
             }
-        }
+        }}
 
         for(Unit u:freeWorkers) meleeUnitBehavior(u, p, gs);
 
